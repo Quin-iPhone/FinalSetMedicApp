d184b2c7f0c822bdc1e2467f757f1af28afde025
Set Medic South Africa <186299622+Quin-iPhone@users.noreply.github.com>
2025-06-29 19:31:17 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,6 +1,9 @@
 from app import app
 from models import db
-
 import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("Database tables created successfully.")
+
+with app.app_context():
+    db.create_all()
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Database tables created successfully.")
+  
